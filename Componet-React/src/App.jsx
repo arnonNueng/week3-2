54b881d4426dd79ac6d609876304ecee39a08d9f
NuengUDT <arnon.wip@sbac.ac.th>
2025-01-24 12:13:17 +0700

    Initial commit

---
+++ Componet-React/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './componets/Header'
+import Heros from './componets/Heros'
+import Feature from './componets/Feature'
+import Footer from './componets/Footer'
+
+const App = () => {
+  return (
+    <>
+        <Header />
+        <Heros />
+        <Feature />
+        <Footer />
+    </>
+  )
+}
+
+export default App
